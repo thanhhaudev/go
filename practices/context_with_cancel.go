03e1dcd5d3560accbd347a4b547ed9605529b1e6
thanhhaudev <thanhhaudev@gmail.com>
2024-09-24 11:04:53 +0700

    context with cancel

--- practices/context_with_cancel.go
+++ practices/context_with_cancel.go
@@ -1,1 +1,34 @@
 package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+)
+
+func doWork(ctx context.Context) {
+	for { // listen for the context to be canceled
+		select {
+		case <-ctx.Done():
+			fmt.Println("context is done:", ctx.Err())
+			return
+		default:
+			fmt.Println("working...")          // this will be printed every 500ms until the context is canceled (4 times for 2 seconds sleep in line 28)
+			time.Sleep(500 * time.Millisecond) // simulate work taking 500ms
+		}
+	}
+}
+
+func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+
+	go doWork(ctx) // start work in a goroutine
+
+	time.Sleep(2 * time.Second) // allow time for work to be done
+
+	fmt.Println("canceling the context...")
+	cancel() // cancel the work
+
+	time.Sleep(1 * time.Second) // allow time for work to be canceled
+	fmt.Println("done")
+}
